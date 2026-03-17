a259e331b7742b2d44026a4b59d20fb5f8aee55d
ChordArc <flqms710@gmail.com>
2026-03-17 18:38:18 +0900

    add PlayerStat

---
+++ Assets/Scripts/Player/PlayerStat.cs
@@ -0,0 +1,15 @@
+public class PlayerStat
+{
+    // POINTS
+    public int Health; // max HP
+    public int Mana; // max MP
+    public int Strength; // Physical damage
+    public int Intelligence; // Magical damage
+    public int Defense; // Physical damage reduction
+    public int Resistance; // Magical damage reduction
+    public int Agility; // Speed
+
+    // Character
+    public int Level;
+    public int Exp;
+}
