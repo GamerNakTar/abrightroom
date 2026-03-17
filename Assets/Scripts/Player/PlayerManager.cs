a29f8b3ee818ea139596376f606910ae06f96b83
ChordArc <flqms710@gmail.com>
2026-03-17 18:39:01 +0900

    add PlayerManager

---
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class PlayerManager : MonoSingleton<PlayerManager>
+{
+    PlayerStat stat;
+
+    void Awake()
+    {
+        stat = SaveSystem.CurSaveData.Stat;
+    }
+
+    void Start()
+    {
+        EventManager<int>.StartListening(Event.GainExp, AddExp);
+    }
+
+    void AddExp(int exp)
+    {
+        stat.Exp += exp;
+        MyLogger.Log($"add {exp} exp, exp: {stat.Exp}", LogType.Info);
+    }
+}
